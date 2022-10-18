ce93ddff91e8417014f0797600719ebe9766f721
SMproductive <81246571+SMproductive@users.noreply.github.com>
2022-10-18 00:34:05 +0000

    add st

--- nordic-channel/packages/suckless.scm
+++ nordic-channel/packages/suckless.scm
@@ -18,3 +18,18 @@
           (base32 "17dldrwf50334sn465nvpak9sbs583h88k8xwsahq90bk18lsl8a"))))
     (home-page "https://github.com/SMproductive/nordic-dwm")
     (synopsis "Nordic themed dwm")))
+
+(define-public nordic-st
+  (package
+    (inherit st)
+    (name "nordic-st")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+          (url "https://github.com/SMproductive/nordic-st")
+          (commit "fac1a497ba1c539c7dda8e23623ad28775137b17")))
+        (sha256
+          (base32 "0iz6074m1104hbxd701wfr8npnrna3r499y0g7zmripbpi3nijs0"))))
+    (home-page "https://github.com/SMproductive/nordic-st")
+    (synopsis "Nordic themed st")))
