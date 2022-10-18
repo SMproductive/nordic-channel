6e907a13c28e56de6b2acf04d6c05f43f24767c6
SMproductive <81246571+SMproductive@users.noreply.github.com>
2022-10-18 00:37:20 +0000

    Added slstatus

--- nordic-channel/packages/suckless.scm
+++ nordic-channel/packages/suckless.scm
@@ -33,3 +33,18 @@
           (base32 "0iz6074m1104hbxd701wfr8npnrna3r499y0g7zmripbpi3nijs0"))))
     (home-page "https://github.com/SMproductive/nordic-st")
     (synopsis "Nordic themed st")))
+
+(define-public nordic-slstatus
+  (package
+    (inherit slstatus)
+    (name "nordic-slstatus")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+          (url "https://github.com/SMproductive/nordic-slstatus")
+          (commit "8b7c93fe3146a98a27dc61cd9abdba8e183c9181")))
+        (sha256
+          (base32 "0svwq3y081vv2sl60v1304rkpx3phkm9ar157dvgp1jm4lvy0qq6"))))
+    (home-page "https://github.com/SMproductive/nordic-slstatus")
+    (synopsis "Simple slstatus for nordic themed dwm")))
