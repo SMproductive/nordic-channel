857c6f0295ac4ee0f5df498c3245ce8ad9f39099
Maximilian Strele <SMproductive@tutanota.de>
2022-12-09 15:18:34 -0500

    added todo

--- nordic-channel/packages/wm.scm
+++ nordic-channel/packages/wm.scm
@@ -9,6 +9,7 @@
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:))
 
+;; TODO desktop file
 (define-public nordic-dwl
   (package
     (inherit dwl)
@@ -21,40 +22,6 @@
           (commit "3756724a882f71767a19abba614dda150d288e26")))
         (sha256
           (base32 "0ac3s134bha8z5ml0m724hhda69j34gma8fqd1zjraj3y5bs0sfz"))))
-    (build-system gnu-build-system)
-    (arguments
-     `(#:tests? #f                      ; no tests
-       #:make-flags
-       (list
-        (string-append "CC=" ,(cc-for-target))
-        (string-append "PREFIX=" (assoc-ref %outputs "out")))
-       #:phases
-       (modify-phases %standard-phases
-         (delete 'configure)
-        (replace 'install
-          (lambda* (#:key outputs #:allow-other-keys)
-            (let ((out (assoc-ref outputs "out")))
-              (invoke "make" "install"
-                      (string-append "DESTDIR=" out) "PREFIX="))))
-        (add-after 'build 'install-wayland-session
-          (lambda* (#:key outputs #:allow-other-keys)
-            ;; Add a .desktop file to wayland-sessions.
-            (let* ((output (assoc-ref outputs "out"))
-                   (wayland-sessions (string-append output "/share/wayland-sessions")))
-              (mkdir-p wayland-sessions)
-              (with-output-to-file
-                  (string-append wayland-sessions "/dwl.desktop")
-                (lambda _
-                  (format #t
-                    "[Desktop Entry]~@
-                     Name=dwl~@
-                     Comment=Dynamic Window Manager~@
-                     Exec=~a/bin/dwl~@
-                     TryExec=~@*~a/bin/dwl~@
-                     Icon=~@
-                     Type=Application~%"
-                    output)))
-              #t))))))
     (home-page "https://github.com/SMproductive/nordic-dwl")
     (synopsis "Nordic themed dwl")))
 
