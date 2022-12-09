f777efe55b28b6b69629dd42f563362bc680f650
Maximilian Strele <SMproductive@tutanota.de>
2022-12-09 14:05:26 -0500

    test

--- nordic-channel/packages/wm.scm
+++ nordic-channel/packages/wm.scm
@@ -21,82 +21,35 @@
           (commit "3756724a882f71767a19abba614dda150d288e26")))
         (sha256
           (base32 "0ac3s134bha8z5ml0m724hhda69j34gma8fqd1zjraj3y5bs0sfz"))))
+    (arguments
+     `(#:tests? #f                      ; no tests
+       #:make-flags
+       (list
+        (string-append "CC=" ,(cc-for-target))
+        (string-append "PREFIX=" (assoc-ref %outputs "out")))
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+
+        (add-after 'build 'install-xsession
+          (lambda* (#:key outputs #:allow-other-keys)
+            ;; Add a .desktop file to xsessions.
+            (let* ((output (assoc-ref outputs "out"))
+                   (xsessions (string-append output "/share/xsessions")))
+              (mkdir-p xsessions)
+              (with-output-to-file
+                  (string-append xsessions "/dwl.desktop")
+                (lambda _
+                  (format #t
+                    "[Desktop Entry]~@
+                     Name=dwl~@
+                     Comment=Dynamic Window Manager~@
+                     Exec=~a/bin/dwl~@
+                     TryExec=~@*~a/bin/dwl~@
+                     Icon=~@
+                     Type=Application~%"
+                    output)))
+              #t))))))         ; no configure
     (home-page "https://github.com/SMproductive/nordic-dwl")
     (synopsis "Nordic themed dwl")))
 
-
-
-
-
-;; (define-public nordic-dwl
-;;   (package
-;;     (name "nordic-dwl")
-;;     (version "0.2.1")
-;;     (source
-;;       (origin
-;;         (method git-fetch)
-;;         (uri (git-reference
-;;           (url "https://github.com/SMproductive/nordic-dwl")
-;;           (commit "09e611b1edb7a39f5eafcca250b9d82a01a69515")))
-;;         (sha256
-;;           (base32 "1bi8vk81nbmgbh379dcgybx1fv22jriybp6lnq58vxz3ijbbgdfz"))))
-
-;;    (build-system gnu-build-system)
-;;     (arguments
-;;      `(#:tests? #f                      ; no tests
-;;        #:make-flags
-;;        (list
-;;         (string-append "CC=" ,(cc-for-target))
-;;         (string-append "PREFIX=" (assoc-ref %outputs "out")))
-;;        #:phases
-
-;;        (modify-phases %standard-phases
-;;          (replace 'configure
-;;            (lambda _
-;;              (substitute* "Makefile" (("\\$\\{CC\\}") "gcc"))
-;;              #t))
-;;         (replace 'install
-;;           (lambda* (#:key outputs #:allow-other-keys)
-;;             (let ((out (assoc-ref outputs "out")))
-;;               (invoke "make" "install"
-;;                       (string-append "DESTDIR=" out) "PREFIX="))))
-;;         (add-after 'build 'install-xsession
-;;           (lambda* (#:key outputs #:allow-other-keys)
-;;             ;; Add a .desktop file to xsessions.
-;;             (let* ((output (assoc-ref outputs "out"))
-;;                    (xsessions (string-append output "/share/xsessions")))
-;;               (mkdir-p xsessions)
-;;               (with-output-to-file
-;;                   (string-append xsessions "/dwl.desktop")
-;;                 (lambda _
-;;                   (format #t
-;;                     "[Desktop Entry]~@
-;;                      Name=dwl~@
-;;                      Comment=Dynamic Window Manager~@
-;;                      Exec=~a/bin/dwl~@
-;;                      TryExec=~@*~a/bin/dwl~@
-;;                      Icon=~@
-;;                      Type=Application~%"
-;;                     output)))
-;;               #t))))))
-
-
-;;        ;; (modify-phases %standard-phases
-;;        ;;   (delete 'configure))))         ; no configure
-;;     (native-inputs
-;;      (list wayland-protocols
-;;            pkg-config))
-;;     (inputs
-;;      (list wayland
-;;            wlroots))
-;;     (home-page "https://github.com/djpohly/dwl")
-;;     (synopsis "Dynamic window manager for Wayland with nordic theme")
-;;     (description
-;;      "@command{dwl} is a compact, hackable compositor for Wayland based on
-;; wlroots.  It is intended to fill the same space in the Wayland world that dwm
-;; does in X11, primarily in terms of philosophy, and secondarily in terms of
-;; functionality.  Like dwm, dwl is easy to understand and hack on, due to a
-;; limited size and a few external dependencies.  It is configurable via
-;; @file{config.h}.")
-;;     ;;             LICENSE       LICENSE.dwm   LICENSE.tinywl
-;;     (license (list license:gpl3+ license:expat license:cc0))))
