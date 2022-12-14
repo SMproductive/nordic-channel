25785af0ee0a5f1a7a2b46c6f95ae6e04c565185
Maximilian Strele <SMproductive@tutanota.de>
2022-12-14 15:37:41 -0500

    test update

--- nordic-channel/packages/wm.scm
+++ nordic-channel/packages/wm.scm
@@ -202,7 +202,9 @@ limited size and a few external dependencies.  It is configurable via
              (commit "b43758588be22f5a417320a085fa712a89159943")))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "15d12fwy90km0nkpjjr77piahiahnybvg6yfdbbzfn5v54hhqgq9"))))))
+        (base32 "15d12fwy90km0nkpjjr77piahiahnybvg6yfdbbzfn5v54hhqgq9"))))
+   (arguments
+    `(#:tests? #f))))
 
 
 (define-public nordic-wlroots
